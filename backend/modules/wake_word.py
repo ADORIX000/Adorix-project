9b6ea0b79474e64e324c705ee5c37617bb70dcae
SahanAdithya <adithyasahan09@gmail.com>
2026-02-13 22:50:43 +0530

    Continue the parts of wake_word.py

--- backend/modules/wake_word.py
+++ backend/modules/wake_word.py
@@ -2,3 +2,12 @@ import pvporcupine
 from pvrecorder import PvRecorder
 import sys
 import os
+
+# Allow importing from the 'core' folder
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from core.config import PICOVOICE_KEY, WAKE_WORD_PATH, AUDIO_DEVICE_INDEX
+
+class WakeWordService:
+    def __init__(self, callback_function):
+        self.callback = callback_function
+        self.is_listening = False
